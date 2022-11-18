106ddc71c7822c605504ffb3284f4d2b0ba0f62c
alexpacheli <alex@pacheli.com.br>
2022-11-17 21:45:26 -0300

    Aplicativo Instagram

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {View, Text, StyleSheet} from 'react-native';
+
+function App() {
+  return(
+    <View>
+      <Text>
+        
+      </Text>
+    </View>
+  )
+}
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
